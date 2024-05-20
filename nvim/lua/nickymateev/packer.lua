bb0865d456dbf76091d62e0c05e84ec610c3aa51
Nikolay Mateev <bgdunkers@gmail.com>
2024-05-20 21:08:23 +0300

    [nvim] Add treesitter context plugin

--- nvim/lua/nickymateev/packer.lua
+++ nvim/lua/nickymateev/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
+    use('nvim-treesitter/nvim-treesitter-context')
     use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
