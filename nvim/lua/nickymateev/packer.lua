d14be715610d481fdb2ade9430054407ee418952
Nikolay Mateev <bgdunkers@gmail.com>
2024-05-25 00:12:21 +0300

    [nvim] Return the color theme to rose-pine

--- nvim/lua/nickymateev/packer.lua
+++ nvim/lua/nickymateev/packer.lua
@@ -20,7 +20,7 @@ return require('packer').startup(function(use)
         as = 'rose-pine',
         config = function()
             require("rose-pine").setup()
-            vim.cmd('colorscheme rose-pine-moon')
+            vim.cmd('colorscheme rose-pine')
         end
     })
 
