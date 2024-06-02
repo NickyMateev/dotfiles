e109fe66e8d514a55f9396bfbcc0b1ea4349dc56
Nikolay Mateev <bgdunkers@gmail.com>
2024-06-02 14:24:57 +0300

    [nvim] Remap telescope live_grep and oldfiles

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader><leader>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>,', builtin.command_history, {})
 vim.keymap.set('n', '<leader>c', builtin.commands, {})
