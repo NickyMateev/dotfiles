2319884af0fa93706a044bbe572020002c773720
Nikolay Mateev <bgdunkers@gmail.com>
2024-06-30 22:08:11 +0300

    [nvim] Replace color theme rose-pine with catppuccin

--- nvim/lua/nickymateev/packer.lua
+++ nvim/lua/nickymateev/packer.lua
@@ -7,22 +7,14 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    use { "catppuccin/nvim", as = "catppuccin" }
+
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
 
     }
-
-
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            require("rose-pine").setup()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
