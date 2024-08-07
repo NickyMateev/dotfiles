ca1d7309d8b161627d6c3f2c4dc357138288126c
Nikolay Mateev <bgdunkers@gmail.com>
2024-08-08 01:03:55 +0300

    [nvim] Revamp dap-ui key mappings

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -9,32 +9,23 @@ require('dapui').setup({
             }
             },
             position = "bottom",
-            size = 10
+            size = 15
         },
         {
             elements = { {
-                id = "repl",
-                size = 0.75
-            }, {
-                id = "scopes",
-                size = 0.25
-            } },
-            position = "bottom",
-            size = 10
-        },
-        {
-            elements = { {
-                id = "repl",
-                size = 0.50
-            }, {
-                id = "scopes",
-                size = 0.25
-            }, {
                 id = "breakpoints",
-                size = 0.25
+                size = 1
+            } },
+            position = "left",
+            size = 30
+        },
+        {
+            elements = { {
+                id = "scopes",
+                size = 1
             } },
             position = "bottom",
-            size = 10
+            size = 20
         },
     },
 })
@@ -42,12 +33,14 @@ require('dapui').setup({
 vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapStopped', { text = '🟢', texthl = '', linehl = '', numhl = '' })
 
+vim.keymap.set('n', '<A-r>', ":DapNew<CR>")
+vim.keymap.set('n', '<leader><leader>', ":lua require'dap'.continue()<CR>")
 
-vim.keymap.set('n', '<leader>du', ":lua require('dapui').close(); require('dapui').toggle({ layout = 1 })<CR>")
-vim.keymap.set('n', '<leader>d0', ":lua require('dapui').close()<CR>")
-vim.keymap.set('n', '<leader>d1', ":lua require('dapui').close(); require('dapui').toggle({ layout = 1 })<CR>")
-vim.keymap.set('n', '<leader>d2', ":lua require('dapui').close(); require('dapui').toggle({ layout = 2 })<CR>")
-vim.keymap.set('n', '<leader>d3', ":lua require('dapui').close(); require('dapui').toggle({ layout = 3 })<CR>")
+vim.keymap.set('n', '<A-0>', ":lua require('dapui').close()<CR>")
+vim.keymap.set('n', '<A-3>', ":lua require('dapui').toggle({ layout = 1 })<CR>")
+vim.keymap.set('n', '<A-4>', ":lua require('dapui').toggle({ layout = 2 })<CR>")
+vim.keymap.set('n', '<A-5>', ":lua require('dapui').toggle({ layout = 3 })<CR>")
+vim.keymap.set('n', '<A-9>', ":DapToggleRepl<CR>")
 
 vim.keymap.set('n', '<leader>dh', ":lua require'dap'.toggle_breakpoint()<CR>")
 
@@ -55,9 +48,6 @@ vim.keymap.set('n', '<S-k>', ":lua require'dap'.step_out()<CR>")
 vim.keymap.set('n', '<S-l>', ":lua require'dap'.step_into()<CR>")
 vim.keymap.set('n', '<S-j>', ":lua require'dap'.step_over()<CR>")
 
-vim.keymap.set('n', '<leader>ds', ":lua require'dap'.stop()<CR>")
-vim.keymap.set('n', '<leader>dn', ":lua require'dap'.continue()<CR>")
-vim.keymap.set('n', '<leader>dr', ":lua require'dap'.repl.open({}, 'vsplit')<CR><C-w>l")
 vim.keymap.set('n', '<leader>de', ":lua require'dap'.set_exception_breakpoints({\"all\"})<CR>")
 
 vim.keymap.set('n', '<leader>di', ":lua require'dap.ui.widgets'.hover()<CR>")
