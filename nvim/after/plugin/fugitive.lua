8131bac162690719be5cc5cfd21174a74a81557d
Nikolay Mateev <bgdunkers@gmail.com>
2024-08-08 00:11:32 +0300

    [nvim] Add toggle Git Status key mapping

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+local function toggleGit()
+  if vim.fn.buflisted(vim.fn.bufname('fugitive:///*/.git//$')) ~= 0 then
+    vim.cmd[[ execute ":bdelete" bufname('fugitive:///*/.git//$') ]]
+  else
+    vim.cmd[[ execute ":Git" ]]
+  end
+end
+
+vim.keymap.set('n', '<A-2>', toggleGit)
