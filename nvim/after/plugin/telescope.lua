4d4a79e5968781127494a14cac08057aa599305c
Nikolay Mateev <bgdunkers@gmail.com>
2024-08-08 00:15:11 +0300

    [nvim] Remap key telescope bindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,16 +4,21 @@ telescope.setup {
         layout_strategy = "vertical",
         layout_config = {
             width = 0.6,
-            height = 0.6,
-            preview_height = 0.65,
+            height = 0.8,
+        },
+        path_display = { "smart" }
+    },
+    pickers = {
+        oldfiles = {
+            cwd_only = true,
         }
     }
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader><leader>', builtin.live_grep, {})
+vim.keymap.set('n', '<A-e>', builtin.oldfiles, {})
+vim.keymap.set('n', '<A-S-f>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>c', builtin.command_history, {})
 vim.keymap.set('n', '<leader>,', builtin.commands, {})
